4194564b2df002dffe855e824215a1bf219eae97
Arif Rahman Hakim <16.9025@stis.ac.id>
2020-03-07 11:29:15 +0700

    fix issue preferences

--- app/src/main/java/com/example/foodsustainabilityapps/Activity/Home.java
+++ app/src/main/java/com/example/foodsustainabilityapps/Activity/Home.java
@@ -11,10 +11,7 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.support.annotation.Nullable;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -23,20 +20,15 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.example.foodsustainabilityapps.Fragment.DeteksiFragment;
-import com.example.foodsustainabilityapps.Fragment.ListFragment;
-import com.example.foodsustainabilityapps.Fragment.PetaFragment;
 import com.example.foodsustainabilityapps.R;
-import com.example.foodsustainabilityapps.ServiceGenerator;
 
-
-import java.util.prefs.Preferences;
 
 public class Home extends AppCompatActivity {
-    TextView deteksi,peta, list, ketDeteksi, ketPeta, ketList;
+    TextView deteksi, peta, list, ketDeteksi, ketPeta, ketList;
     private LinearLayout linearDeteksi, linearPeta, linearList;
     private ImageView logoDeteksi, logoPeta, logoList;
     private Button mulaiBtn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,51 +46,13 @@ public class Home extends AppCompatActivity {
         linearPeta = findViewById(R.id.linear_peta);
         linearList = findViewById(R.id.linear_list);
 //        mulaiBtn = findViewById(R.id.mulaideteksi);
-        
-        Preference preference = new Preference(this);
 
-        if(preference.getRegisteredUser(this)){
-////
+
+        if (Preferences.getStatusNotifikasi(this)) {
             showNotif();
-////
-            preference.setRegisteredUser(this, false);
+            Preferences.setStatusNotifkasi(this, false);
         }
 
-
-        public void showNotif() {
-            String NOTIFICATION_CHANNEL_ID = "channel_androidnotif";
-            Context context = this.getApplicationContext();
-            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                String channelName = "Android Notif Channel";
-                int importance = NotificationManager.IMPORTANCE_HIGH;
-
-                NotificationChannel mChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, importance);
-                notificationManager.createNotificationChannel(mChannel);
-            }
-
-            Intent mIntent = new Intent(this, MainActivity.class);
-            Bundle bundle = new Bundle();
-            bundle.putString("fromnotif", "notif");
-            mIntent.putExtras(bundle);
-            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, mIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this,NOTIFICATION_CHANNEL_ID);
-            builder.setContentIntent(pendingIntent)
-                    .setSmallIcon(R.drawable.ic_launcher_foreground)
-                    .setLargeIcon(BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_launcher_foreground))
-                    .setTicker("notif starting")
-                    .setAutoCancel(true)
-                    .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
-                    .setLights(Color.RED, 3000, 3000)
-                    .setDefaults(Notification.DEFAULT_SOUND)
-                    .setContentTitle("Notification Android")
-                    .setContentText("by imamfarisi.com");
-
-            notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-            notificationManager.notify(115, builder.build());
-        }
-        
 
         linearList.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -116,5 +70,39 @@ public class Home extends AppCompatActivity {
         });
 
 
+    }
+
+    public void showNotif() {
+        String NOTIFICATION_CHANNEL_ID = "channel_androidnotif";
+        Context context = this.getApplicationContext();
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelName = "Android Notif Channel";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+
+            NotificationChannel mChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, importance);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        Intent mIntent = new Intent(this, MainActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("fromnotif", "notif");
+        mIntent.putExtras(bundle);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, mIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        builder.setContentIntent(pendingIntent)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setLargeIcon(BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_launcher_foreground))
+                .setTicker("notif starting")
+                .setAutoCancel(true)
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                .setLights(Color.RED, 3000, 3000)
+                .setDefaults(Notification.DEFAULT_SOUND)
+                .setContentTitle("Notification Android")
+                .setContentText("by imamfarisi.com");
+
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(115, builder.build());
     }
 }
--- app/src/main/java/com/example/foodsustainabilityapps/Activity/Preferences.java
+++ app/src/main/java/com/example/foodsustainabilityapps/Activity/Preferences.java
@@ -1,3 +1,4 @@
+package com.example.foodsustainabilityapps.Activity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -6,71 +7,19 @@ import android.preference.PreferenceManager;
 
 public class Preferences {
 
-    /** Pendeklarasian key-data berupa String, untuk sebagai wadah penyimpanan data.
-     * Jadi setiap data mempunyai key yang berbeda satu sama lain */
-    static final String KEY_USER_TEREGISTER ="user", KEY_PASS_TEREGISTER ="pass";
-    static final String KEY_USERNAME_SEDANG_LOGIN = "Username_logged_in";
-    static final String KEY_STATUS_SEDANG_LOGIN = "Status_logged_in";
+    private static final String KEY_STATUS_notifikasi = "status_notifkasi";
 
-    /** Pendlakarasian Shared Preferences yang berdasarkan paramater context */
-    private static SharedPreferences getSharedPreference(Context context){
+    private static SharedPreferences getSharedPreference(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context);
     }
 
-    /** Deklarasi Edit Preferences dan mengubah data
-     *  yang memiliki key isi KEY_USER_TEREGISTER dengan parameter username */
-    public static void setRegisteredUser(Context context, String username){
+    static void setStatusNotifkasi(Context context, Boolean status) {
         SharedPreferences.Editor editor = getSharedPreference(context).edit();
-        editor.putString(KEY_USER_TEREGISTER, username);
+        editor.putBoolean(KEY_STATUS_notifikasi, status);
         editor.apply();
-    }
-    /** Mengembalikan nilai dari key KEY_USER_TEREGISTER berupa String */
-    public static String getRegisteredUser(Context context){
-        return getSharedPreference(context).getString(KEY_USER_TEREGISTER,"");
     }
 
-    /** Deklarasi Edit Preferences dan mengubah data
-     *  yang memiliki key KEY_PASS_TEREGISTER dengan parameter password */
-    public static void setRegisteredPass(Context context, String password){
-        SharedPreferences.Editor editor = getSharedPreference(context).edit();
-        editor.putString(KEY_PASS_TEREGISTER, password);
-        editor.apply();
-    }
-    /** Mengembalikan nilai dari key KEY_PASS_TEREGISTER berupa String */
-    public static String getRegisteredPass(Context context){
-        return getSharedPreference(context).getString(KEY_PASS_TEREGISTER,"");
-    }
-
-    /** Deklarasi Edit Preferences dan mengubah data
-     *  yang memiliki key KEY_USERNAME_SEDANG_LOGIN dengan parameter username */
-    public static void setLoggedInUser(Context context, String username){
-        SharedPreferences.Editor editor = getSharedPreference(context).edit();
-        editor.putString(KEY_USERNAME_SEDANG_LOGIN, username);
-        editor.apply();
-    }
-    /** Mengembalikan nilai dari key KEY_USERNAME_SEDANG_LOGIN berupa String */
-    public static String getLoggedInUser(Context context){
-        return getSharedPreference(context).getString(KEY_USERNAME_SEDANG_LOGIN,"");
-    }
-
-    /** Deklarasi Edit Preferences dan mengubah data
-     *  yang memiliki key KEY_STATUS_SEDANG_LOGIN dengan parameter status */
-    public static void setLoggedInStatus(Context context, boolean status){
-        SharedPreferences.Editor editor = getSharedPreference(context).edit();
-        editor.putBoolean(KEY_STATUS_SEDANG_LOGIN,status);
-        editor.apply();
-    }
-    /** Mengembalikan nilai dari key KEY_STATUS_SEDANG_LOGIN berupa boolean */
-    public static boolean getLoggedInStatus(Context context){
-        return getSharedPreference(context).getBoolean(KEY_STATUS_SEDANG_LOGIN,false);
-    }
-
-    /** Deklarasi Edit Preferences dan menghapus data, sehingga menjadikannya bernilai default
-     *  khusus data yang memiliki key KEY_USERNAME_SEDANG_LOGIN dan KEY_STATUS_SEDANG_LOGIN */
-    public static void clearLoggedInUser (Context context){
-        SharedPreferences.Editor editor = getSharedPreference(context).edit();
-        editor.remove(KEY_USERNAME_SEDANG_LOGIN);
-        editor.remove(KEY_STATUS_SEDANG_LOGIN);
-        editor.apply();
+    static Boolean getStatusNotifikasi(Context context) {
+        return getSharedPreference(context).getBoolean(KEY_STATUS_notifikasi, true);
     }
 }
